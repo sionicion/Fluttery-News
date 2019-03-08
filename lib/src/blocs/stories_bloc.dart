80fb31a9e27a692db6ae4280d3b536549fb68e12
Tyler Swindell <electromagnetic.jolt@gmail.com>
2019-03-08 05:15:58 -0500

    minor changes

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -1,20 +1,36 @@
 import 'package:rxdart/rxdart.dart';
 import '../models/item_model.dart';
 import '../resources/repository.dart';
+import 'dart:async';
 
 class StoriesBloc {
   final _repository =Repository();
   final _topIds = PublishSubject<List<int>>();
+  final _items = BehaviorSubject<int>();
 
   // Getters to Streams
   Observable<List<int>> get topIds => _topIds.stream;
+
+  // Getters to Sinks
+  Function(int) get fetchItem => _items.sink.add;
 
   fetchTopIds() async {
     final ids = await _repository.fetchTopIds();
     _topIds.sink.add(ids);
   }
 
+  _itemsTransformer() {
+return ScanStreamTransformer(
+  (Map<int, Future<ItemModel>> cache, int id, _) {
+    cache[id] =_repository.fetchItem(id);
+    return cache;
+  },
+  <int, Future<ItemModel>>{},
+);
+  }
+
   dispose() {
     _topIds.close();
+    _items.close();
   }
 }
